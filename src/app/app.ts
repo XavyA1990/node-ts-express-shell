f9a595b0323e7333ba231ed79bc3ec79769ffb96
XavyA1990 <contrerasdeveloper@outlook.com>
2025-07-16 16:49:02 -0500

    add compression, cors, and helmet middleware to enhance security and performance

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import healthRoute from "./../routes/health.route";
 import { loggerMiddleware } from "../middlewares/logger.middleware";
+import compression from "compression";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 app.use(loggerMiddleware);
+app.use(compression());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
